4b329f8fb5a39da1aa6a45284f2a340579054ceb
Mathy17 <psmathy23@gmail.com>
2026-07-11 19:14:11 +0530

    Add files via upload

---
+++ star patttern.py
@@ -0,0 +1,26 @@
+Python 3.13.14 (tags/v3.13.14:fd17997, Jun 10 2026, 13:03:48) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+============= RESTART: C:/Users/acer/Documents/pgm/star patttern.py ============
+*
+**
+***
+****
+*****
+>>> 
+============= RESTART: C:/Users/acer/Documents/pgm/star patttern.py ============
+*
+**
+***
+****
+*****
+2
+4
+6
+8
+10
+12
+14
+16
+18
+20
