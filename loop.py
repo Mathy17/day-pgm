bd3e84be77dd3f9752dae4b4599e951f6c1a581c
Mathy17 <psmathy23@gmail.com>
2026-07-12 20:30:40 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,17 @@
+for i in range(1,11):
+    print(i)
+for i in range(1,10):
+    i=i*i
+    print(i)
+    
+sum=0    
+for i in range(1,101):
+    sum=sum+i
+    print("sum=",sum)
+
+for i in range(1,51,2):
+    print(i)
+
+for i in range(1,6):
+    print("*"*i)
+    
