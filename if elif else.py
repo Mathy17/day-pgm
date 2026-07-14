a182a54b29e1326446d90d2d440210a24eb21498
Mathy17 <psmathy23@gmail.com>
2026-07-14 20:01:09 +0530

    Add files via upload

---
+++ if elif else.py
@@ -0,0 +1,28 @@
+Python 3.13.14 (tags/v3.13.14:fd17997, Jun 10 2026, 13:03:48) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+============= RESTART: C:/Users/acer/Documents/pgm/if elif else.py =============
+Enter a number: 100
+Positive Number
+>>> 45
+45
+>>> 
+>>> -32
+-32
+>>> 
+============= RESTART: C:/Users/acer/Documents/pgm/if elif else.py =============
+Enter a number: 60
+Positive Number
+a,b,c10,45,60
+Traceback (most recent call last):
+  File "C:/Users/acer/Documents/pgm/if elif else.py", line 10, in <module>
+    num=int(input("a,b,c"))
+ValueError: invalid literal for int() with base 10: '10,45,60'
+>>> 
+============= RESTART: C:/Users/acer/Documents/pgm/if elif else.py =============
+Enter a number: 56
+Positive Number
+Enter your marks:76
+Grade B
+>>> 67
+67
