acea249208db3047de31dd8ff50d505d23e8c5e7
Mathy17 <psmathy23@gmail.com>
2026-07-15 19:35:19 +0530

    Add files via upload

--- if elif else.py
+++ if elif else.py
@@ -1,2 +1,19 @@
+num = int(input("Enter a number: "))
 
-    
+if num > 0:
+    print("Positive Number")
+elif num < 0:
+    print("Negative Number")
+else:
+    print("Zero")
+
+marks=int(input("Enter your marks:"))
+
+if marks>=90:
+        print("Grade A")
+elif marks>=75:
+        print("Grade B")
+elif marks>=50:
+    print("Grade C")
+else:
+    print("Fail")
